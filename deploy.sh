c1a1412cf5a2a8477eedd9880753d1f346e73845
bep <bjorn.erik.pedersen@gmail.com>
2014-11-02 00:25:38 +0100

    Prepare new deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,118 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit #abort if any command fails
 
-function info { echo -e '\033[1;34m'"$1"'\033[0m'; }
-function warn { echo -e '\033[1;33m'"$1"'\033[0m'; }
-function err { echo -e '\033[1;31mERROR: '"$1"'\033[0m'; }
+deploy_directory=dist
+deploy_branch=gh-pages
 
-die () {
-    err >&2 "$@"
-    exit 1
+#if no user identity is already set in the current git environment, use this:
+default_username=deploy.sh
+default_email=
+
+#repository to deploy to. must be readable and writable.
+repo=origin
+
+# Parse arg flags
+while : ; do
+	if [[ $1 = "-v" || $1 = "--verbose" ]]; then
+		verbose=true
+		shift
+	elif [[ $1 = "-s" || $1 = "--setup" ]]; then
+		setup=true
+		shift
+	else
+		break
+	fi
+done
+
+#echo expanded commands as they are executed (for debugging)
+function enable_expanded_output {
+	if [ $verbose ]; then
+		set -o xtrace
+		set +o verbose
+	fi
 }
 
+#this is used to avoid outputting the repo URL, which may contain a secret token
+function disable_expanded_output {
+	if [ $verbose ]; then
+		set +o xtrace
+		set -o verbose
+	fi
+}
 
-# Import my dev env
-. $HOME/dev_settings.sh
+enable_expanded_output
 
-DESTINATION=$HOME/dev/bepsays.com/public
-SOURCE=$HOME/dev/bepsays.com
-LOG=$HOME/dev/log/hugobuild.log
+function set_user_id {
+	if [[ -z `git config user.name` ]]; then
+		git config user.name "$default_username"
+	fi
+	if [[ -z `git config user.email` ]]; then
+		git config user.email "$default_email"
+	fi
+}
 
-[ "$#" -eq 1 ] || die "Commit message required."
+function restore_head {
+	if [[ $previous_branch = "HEAD" ]]; then
+		#we weren't on any branch before, so just set HEAD back to the commit it was on
+		git update-ref --no-deref HEAD $commit_hash $deploy_branch
+	else
+		git symbolic-ref HEAD refs/heads/$previous_branch
+	fi
+	
+	git reset --mixed
+}
 
-info "Cleaning build folder $DESTINATION ... "
-rm -rf $DESTINATION || die "Failed to remove destination."
+commit_title=`git log -n 1 --format="%s" HEAD`
+commit_hash=`git log -n 1 --format="%H" HEAD`
+previous_branch=`git rev-parse --abbrev-ref HEAD`
 
-info "Building static files ..."
-grunt build || die "Failed to build static files"
-info "Building site ..."
-hugo --source="$SOURCE" --destination="$DESTINATION" --logFile="$LOG" || die "Failed to build site"
-
-# safe guard - should never happen
-find public -name "*.html" | xargs grep -q $DEV_DOMAIN
-if [ "$?" -eq "0" ]
-    then die "Site contains local settings"
+if [ $setup ]; then
+	mkdir -p $deploy_directory
+	git --work-tree $deploy_directory checkout --orphan $deploy_branch
+	git --work-tree $deploy_directory rm -r "*"
+	git --work-tree $deploy_directory add --all
+	git --work-tree $deploy_directory commit -m "initial publish"$'\n\n'"generated from commit $commit_hash"
+	git push $repo $deploy_branch
+	restore_head
+	exit
 fi
 
-info "Deploy to GitHub ..."
-{ git add -A && git commit -m "$1" && git push origin master && git subtree push --prefix=public origin gh-pages;} || die "Failed to push to Git. Nothing changed?"
+if ! git diff --exit-code --quiet --cached; then
+	echo Aborting due to uncommitted changes in the index >&2
+	exit 1
+fi
 
-# get it into dev shape again
-grunt dev
+disable_expanded_output
+git fetch --force $repo $deploy_branch:$deploy_branch
+enable_expanded_output
 
-info "****************** Done! *********************"
+#make deploy_branch the current branch
+git symbolic-ref HEAD refs/heads/$deploy_branch
 
+#put the previously committed contents of deploy_branch branch into the index
+git --work-tree "$deploy_directory" reset --mixed --quiet
 
+git --work-tree "$deploy_directory" add --all
 
+set +o errexit
+diff=$(git --work-tree "$deploy_directory" diff --exit-code --quiet HEAD)$?
+set -o errexit
+case $diff in
+	0) echo No changes to files in $deploy_directory. Skipping commit.;;
+	1)
+		set_user_id
+		git --work-tree "$deploy_directory" commit -m \
+			"publish: $commit_title"$'\n\n'"generated from commit $commit_hash"
+
+		disable_expanded_output
+		#--quiet is important here to avoid outputting the repo URL, which may contain a secret token
+		git push --quiet $repo $deploy_branch
+		enable_expanded_output
+		;;
+	*)
+		echo git diff exited with code $diff. Aborting. Staying on branch $deploy_branch so you can debug. To switch back to master, use: git symbolic-ref HEAD refs/heads/master && git reset --mixed >&2
+		exit $diff
+		;;
+esac
+
+restore_head
---
+++ publish.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function info { echo -e '\033[1;34m'"$1"'\033[0m'; }
+function warn { echo -e '\033[1;33m'"$1"'\033[0m'; }
+function err { echo -e '\033[1;31mERROR: '"$1"'\033[0m'; }
+
+die () {
+    err >&2 "$@"
+    exit 1
+}
+
+
+# Import my dev env
+. $HOME/dev_settings.sh
+
+DESTINATION=$HOME/dev/bepsays.com/dist
+SOURCE=$HOME/dev/bepsays.com
+LOG=$HOME/dev/log/hugobuild.log
+
+#[ "$#" -eq 1 ] || die "Commit message required."
+
+info "Cleaning build folder $DESTINATION ... "
+rm -rf $DESTINATION || die "Failed to remove destination."
+
+info "Building static files ..."
+grunt build || die "Failed to build static files"
+info "Building site ..."
+hugo --source="$SOURCE" --destination="$DESTINATION" --logFile="$LOG" || die "Failed to build site"
+
+# safe guard - should never happen
+find $DESTINATION -name "*.html" | xargs grep -q $DEV_DOMAIN
+if [ "$?" -eq "0" ]
+    then die "Site contains local settings"
+fi
+
+info "Deployment to GitHub ... "
+
+./deploy.sh || die "Deployment failed."
+
+info "****************** Done! *********************"
+
+
+
