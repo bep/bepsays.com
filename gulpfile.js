3a94defcfa0ab23cb015574b6deabab5247bb06a
bep <bjorn.erik.pedersen@gmail.com>
2014-11-28 18:19:21 +0100

    Add cache header for css etc.

--- gulpfile.js
+++ gulpfile.js
@@ -70,6 +70,39 @@ gulp.task('aws-publish', ['build'], function () {
     var publisher = awspublish.create(JSON.parse(fs.readFileSync(process.env.HOME + '/.aws/bepsays.json')));
 
     return gulp.src('./dist/**')
+        .pipe(awspublishRouter({
+            cache: {
+                // cache for 5 minutes by default
+                cacheTime: 300
+            },
+
+            routes: {
+                "^assets/(?:.+)\\.(?:js|css|svg|ttf)$": {
+                    // don't modify original key. this is the default
+                    key: "$&",
+                    // use gzip for assets that benefit from it
+                    gzip: true,
+                    // cache static assets for 2 years
+                    cacheTime: 630720000
+                },
+
+                "^assets/.+$": {
+                    // cache static assets for 2 years
+                    cacheTime: 630720000
+                },
+
+
+                "^README$": {
+                    // specify extra headers
+                    headers: {
+                        "Content-Type": "text/plain"
+                    }
+                },
+
+                // pass-through for anything that wasn't matched by routes above, to be uploaded with default options
+                "^.+$": "$&"
+            }
+        }))
         .pipe(parallelize(publisher.publish(), 10))
         .pipe(publisher.sync())
         .pipe(publisher.cache())
@@ -81,11 +114,10 @@ gulp.task('aws-publish', ['build'], function () {
 
 gulp.task('deploy', ['aws-publish'], function () {
 
-   // get it back into dev shape again
+    // get it back into dev shape again
     return copyPartials();
 
 });
-
 
 
 gulp.task('watch', ['copy', 'less', 'scripts'], function () {
