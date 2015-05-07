7e818aeed530825e09886d820cf11d1afcf9858d
bep <bjorn.erik.pedersen@gmail.com>
2015-05-07 19:00:03 +0200

    Generalized Gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var requireDir = require('require-dir');
+var env = require('gulp-env');
+
+env({
+    vars: {
+        site: "bepsays"
+    }
+});
+
 
 // Require all tasks in gulp, including subfolders
-requireDir('./gulp', { recurse: true });
+requireDir('./gulp', {recurse: true});
