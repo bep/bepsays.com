76bfbab39c3995e28a75410b3fd66806038101f3
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-01-10 12:33:35 +0100

    Update deploy script to latest s3deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 rm -rf public
 hugo
-s3deploy -source=public/ -region=eu-west-1 -bucket=bepsays.com -distribution-id=E18C62T0HLIL27
+s3deploy -source=public/ -region=eu-west-1 -bucket=bepsays.com -public-access -distribution-id=E18C62T0HLIL27
